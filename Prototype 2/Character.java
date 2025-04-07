c2d3de8a71ee65fd51b7c03ec3ae319af1cb03a0
TAvital04 <108590031+TAvital04@users.noreply.github.com>
2025-04-06 22:43:12 -0400

    Finihs maze

---
+++ Prototype 2/Character.java
@@ -0,0 +1,21 @@
+// Imports
+import java.awt.event.*;
+
+// Class
+public class Character
+{
+    // Declare variables
+
+
+    // Constructor
+    public Character(int x, int y)
+    {
+        
+    }
+
+    // Functions
+    public void keyPressed(KeyEvent e)
+    {
+        System.out.println(e);
+    }
+}
